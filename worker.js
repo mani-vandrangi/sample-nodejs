3e0000a04048ef97ff56480f6b68f37ffd02cd09
Mani Deepak Vandrangi <mani.vandrangi@freshworks.com>
2020-12-21 17:21:39 +0530

    Axios test

--- worker.js
+++ worker.js
@@ -1,7 +1,11 @@
 'use strict';
 
-function logSample() {
+const axios = request('axios');
+
+async function logSample() {
   console.log('worker');
+  const response = await axios.get('http://sample-nodejs');
+  console.log(response);
 }
 
 setInterval(logSample, 10000);
