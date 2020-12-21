4e2e0593de4e80f7dca8febfb85fe2f166b4ce23
Mani Deepak Vandrangi <mani.vandrangi@freshworks.com>
2020-12-21 17:28:31 +0530

    Decreasing frequency

--- worker.js
+++ worker.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 async function logSample() {
   console.log('worker');
   const response = await axios.get('http://sample-nodejs');
-  console.log(response);
+  console.log(response.data);
 }
 
-setInterval(logSample, 10000);
+setInterval(logSample, 60000);
